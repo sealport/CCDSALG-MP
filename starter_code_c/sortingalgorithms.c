393ceeee047f7356e47fbcb6adf3318b0a5005e1
sealport <john_benedict_garcia@dlsu.edu.ph>
2025-07-01 17:06:41 +0800

    Added selection sort and swap function

--- starter_code_c/sortingalgorithms.c
+++ starter_code_c/sortingalgorithms.c
@@ -25,15 +25,35 @@ void insertionSort(Record *arr, int n)
 
 }
 
+void swap(Record *a, Record *b) 
+{
+    Record temp;
+
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 void selectionSort(Record *arr, int n)
 {
-    // TODO: Implement this sorting algorithm here.
+    int idxCurrent;
+    int idxLowest;
+    int i, j;
 
+    for(i = 0; i < n; i++) {
+        idxLowest = i;
+        
+        for(j = i+1; j < n; j++) {
+            idxCurrent = j;
+            if (arr[idxLowest].idNumber > arr[idxCurrent].idNumber) {
+                idxLowest = idxCurrent;
+            }
+        }
 
-
-
-
-
+        if (idxLowest != i) {
+            swap(&arr[i],&arr[idxLowest]);
+        } 
+    }
 }
 
 void mergeSort(Record *arr, int p, int r)
