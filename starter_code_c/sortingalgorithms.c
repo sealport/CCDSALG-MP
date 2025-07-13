1bed4b16023b101e38f648195aa4d9e05c837ca6
sealport <john_benedict_garcia@dlsu.edu.ph>
2025-07-13 15:21:42 +0800

    WORKING MERGE

--- starter_code_c/sortingalgorithms.c
+++ starter_code_c/sortingalgorithms.c
@@ -9,6 +9,47 @@
 * as needed by the sorting algorithms here.
 */
 
+#define MAX_RECORDS 100000
+Record mergeTemp[MAX_RECORDS];
+
+
+// mergeSort helper - sorting subarrays
+void merge(Record *arr, int p, int q, int r)
+{
+	int i = p;
+	int j = q + 1;
+	int k = p;
+
+	while (i <= q && j <= r) {
+		if (arr[i].idNumber <= arr[j].idNumber) {
+			mergeTemp[k] = arr[i];
+			k++;
+			i++;
+		} else {
+			mergeTemp[k] = arr[j];
+			k++;
+			j++;
+		}
+	}
+
+	while (i <= q) {
+		mergeTemp[k] = arr[i];
+		k++;
+		i++;
+	}
+
+	while (j <= r ) {
+		mergeTemp[k] = arr[j];
+		k++;
+		j++;
+	}
+
+	for (i = p; i <= r; i++) {
+		arr[i] = mergeTemp[i];
+	}
+}
+
+// swap helper function 
 void swapRecords(Record *a, Record *b) 
 {
     Record temp;
@@ -18,117 +59,77 @@ void swapRecords(Record *a, Record *b)
     *b = temp;
 }
 
-void merge(Record *arr, int left, int middle, int right)
-{
-	int i = 0, j = 0, k = left;
-	int n1 = middle - left + 1;
-	int n2 = right - middle;
-	int L[n1], R[n2];
-	
-    for (i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[middle + 1 + j];
-
-	while (i < n1 && j < n2) 
-	{
-		if (L[i] >= R[j]) {
-			arr[k] = L[i];
-			i++;
-		}
-		else {
-			arr[k] = R[j];
-			j++;
-		}
-		k++;
-	}
-	
-	while (i < n1) {
-		arr[k] = L[i];
-		i++;
-		k++;
-	}
-	
-	while (j < n2) {
-		arr[k] = R[j];
-		j++;
-		k++;
-	}
-}
 
 void insertionSort(Record *arr, int n)
 {
+    // TODO: Implement this sorting algorithm here.
     int i, j;
-    Record nKey;
+    Record key;
+	
+    for (i = 1; i < n; i++) {
+    	key = arr[i];
+    	j = i - 1;		// J is 1 index less than i
+    	
+    	// Compares index i with index j & swaps if conditions are met
+    	while (j >= 0 && arr[j].idNumber > key.idNumber) {
+    		arr[j+1] = arr[j];
+    		j--;
+		}
+		
+		arr[j+1] = key;
+	}
 
-
-    for (i = 1 ; i < n ; i++)
-    {
-        nKey = arr[i];
-        j = i - 1;
-
-
-        while (j >= 0 && arr[j].idNumber > nKey.idNumber)
-        {
-            arr[j + 1] = arr[j]; // Shift right
-            j--;
-        }
-
-
-        arr[j + 1] = nKey;
-    }
 }
-
 
 void selectionSort(Record *arr, int n)
 {
-    int idxCurrent;
-    int idxLowest;
-    int i, j;
-
-    for(i = 0; i < n; i++) {
-        idxLowest = i;
-        
-        for(j = i+1; j < n; j++) {
-            idxCurrent = j;
-            if (arr[idxLowest].idNumber > arr[idxCurrent].idNumber) {
-                idxLowest = idxCurrent;
+    // TODO: Implement this sorting algorithm here.
+	int i, j, min_index;
+    Record temp;
+	
+    for (i = 0; i < n - 1; i++) {
+		min_index = i;
+		
+		// Cycles through array and updates min_index if smaller number is found
+		for (j = i + 1; j < n; j++) {
+            if (arr[j].idNumber < arr[min_index].idNumber) {
+                min_index = j;
             }
         }
+        
+        // Swap min_index to correct location if it has been updated
+		if (min_index != i) {
+			temp = arr[i];
+        	arr[i] = arr[min_index];
+        	arr[min_index] = temp;
+		}
+	}
 
-        if (idxLowest != i) {
-            swapRecords(&arr[i],&arr[idxLowest]);
-        } 
-    }
 }
 
-void mergeSort(Record *arr, int l, int r)
+void mergeSort(Record *arr, int p, int r)
 {
     // TODO: Implement this sorting algorithm here.
-	int m;
-
-
-	if (l < r) {
-		m = l + (r - l) / 2;
-		mergeSort(arr, l, m);
-		mergeSort(arr, m + 1, r);
-		merge(arr, l, m, r);
+	if (p < r) {
+		int q = p + (r - p) / 2;
+		mergeSort(arr, p, q);
+		mergeSort(arr, q + 1, r);
+		merge(arr, p, q, r);
 	}
 }
-
 
 /*
 * Define AT LEAST ONE more sorting algorithm here, apart from the
 * ones given above. Make sure that the method accepts an array of
 * record structures.
 */
-void bubbleSort(Record *arr, int nSize)
+void bubbleSort(Record *arr, int n)
 {
     int i, j;
 
-    for (i = 0 ; i < nSize ; i++)
+    for (i = 0 ; i < n ; i++)
     {
-        for (j = nSize - 1 ; j >= i + 1 ; j--)
+        for (j = n - 1 ; j >= i + 1 ; j--)
         {
             if (arr[j].idNumber < arr[j - 1].idNumber)
             {
@@ -137,8 +138,6 @@ void bubbleSort(Record *arr, int nSize)
         }
     }
 }
-
-
 
 
 
